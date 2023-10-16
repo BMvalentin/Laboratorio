bd35efbefae68d3ab4ac5c04ac5f15ebfce240e3
rociobuono <91210086+rociobuono@users.noreply.github.com>
2023-10-16 16:37:40 -0300

    puse los componentes dentro de paneles y modifique las propiedades anchor

--- Laboratorio/Form1.Designer.cs
+++ Laboratorio/Form1.Designer.cs
@@ -41,27 +41,27 @@
             this.txtTelefono = new System.Windows.Forms.TextBox();
             this.txtApellido = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
-
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridPacientes)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(21, 132);
+            this.btnEliminar.Location = new System.Drawing.Point(12, 129);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(83, 22);
             this.btnEliminar.TabIndex = 1;
@@ -70,7 +70,7 @@
             // 
             // Modificar
             // 
-            this.Modificar.Location = new System.Drawing.Point(21, 87);
+            this.Modificar.Location = new System.Drawing.Point(12, 84);
             this.Modificar.Name = "Modificar";
             this.Modificar.Size = new System.Drawing.Size(83, 23);
             this.Modificar.TabIndex = 2;
@@ -80,28 +80,16 @@
             // btnAgregar
             // 
             this.btnAgregar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnAgregar.Location = new System.Drawing.Point(21, 47);
+            this.btnAgregar.Location = new System.Drawing.Point(12, 44);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(83, 23);
             this.btnAgregar.TabIndex = 0;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
-            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
-
-            // panel2
             // 
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(200, 350);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(600, 100);
-            this.panel2.TabIndex = 1;
-            // 
-
             // dataGridPacientes
             // 
+            this.dataGridPacientes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.dataGridPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridPacientes.Location = new System.Drawing.Point(120, 0);
             this.dataGridPacientes.Name = "dataGridPacientes";
@@ -111,7 +99,7 @@
             // lblTelefono
             // 
             this.lblTelefono.AutoSize = true;
-            this.lblTelefono.Location = new System.Drawing.Point(621, 168);
+            this.lblTelefono.Location = new System.Drawing.Point(5, 165);
             this.lblTelefono.Name = "lblTelefono";
             this.lblTelefono.Size = new System.Drawing.Size(49, 13);
             this.lblTelefono.TabIndex = 9;
@@ -120,7 +108,7 @@
             // lblDNI
             // 
             this.lblDNI.AutoSize = true;
-            this.lblDNI.Location = new System.Drawing.Point(621, 126);
+            this.lblDNI.Location = new System.Drawing.Point(5, 123);
             this.lblDNI.Name = "lblDNI";
             this.lblDNI.Size = new System.Drawing.Size(26, 13);
             this.lblDNI.TabIndex = 8;
@@ -129,7 +117,7 @@
             // lblApellido
             // 
             this.lblApellido.AutoSize = true;
-            this.lblApellido.Location = new System.Drawing.Point(621, 87);
+            this.lblApellido.Location = new System.Drawing.Point(5, 84);
             this.lblApellido.Name = "lblApellido";
             this.lblApellido.Size = new System.Drawing.Size(44, 13);
             this.lblApellido.TabIndex = 7;
@@ -137,7 +125,7 @@
             // 
             // txtDni
             // 
-            this.txtDni.Location = new System.Drawing.Point(681, 123);
+            this.txtDni.Location = new System.Drawing.Point(65, 120);
             this.txtDni.Name = "txtDni";
             this.txtDni.Size = new System.Drawing.Size(100, 20);
             this.txtDni.TabIndex = 6;
@@ -145,7 +133,7 @@
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(619, 50);
+            this.lblNombre.Location = new System.Drawing.Point(3, 47);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(44, 13);
             this.lblNombre.TabIndex = 5;
@@ -154,7 +142,7 @@
             // lblDatosPersonales
             // 
             this.lblDatosPersonales.AutoSize = true;
-            this.lblDatosPersonales.Location = new System.Drawing.Point(661, 21);
+            this.lblDatosPersonales.Location = new System.Drawing.Point(45, 18);
             this.lblDatosPersonales.Name = "lblDatosPersonales";
             this.lblDatosPersonales.Size = new System.Drawing.Size(90, 13);
             this.lblDatosPersonales.TabIndex = 4;
@@ -162,29 +150,30 @@
             // 
             // txtTelefono
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(681, 165);
+            this.txtTelefono.Location = new System.Drawing.Point(65, 162);
             this.txtTelefono.Name = "txtTelefono";
             this.txtTelefono.Size = new System.Drawing.Size(100, 20);
             this.txtTelefono.TabIndex = 3;
             // 
             // txtApellido
             // 
-            this.txtApellido.Location = new System.Drawing.Point(681, 84);
+            this.txtApellido.Location = new System.Drawing.Point(65, 81);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(100, 20);
             this.txtApellido.TabIndex = 1;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(681, 47);
+            this.txtNombre.Location = new System.Drawing.Point(65, 44);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(100, 20);
             this.txtNombre.TabIndex = 0;
             // 
-
             // tabControl1
             // 
-            this.tabControl1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
@@ -196,19 +185,9 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.lblTelefono);
-            this.tabPage1.Controls.Add(this.btnEliminar);
-            this.tabPage1.Controls.Add(this.lblDNI);
-            this.tabPage1.Controls.Add(this.lblApellido);
+            this.tabPage1.Controls.Add(this.panel3);
+            this.tabPage1.Controls.Add(this.panel1);
             this.tabPage1.Controls.Add(this.dataGridPacientes);
-            this.tabPage1.Controls.Add(this.txtDni);
-            this.tabPage1.Controls.Add(this.Modificar);
-            this.tabPage1.Controls.Add(this.lblNombre);
-            this.tabPage1.Controls.Add(this.btnAgregar);
-            this.tabPage1.Controls.Add(this.lblDatosPersonales);
-            this.tabPage1.Controls.Add(this.txtTelefono);
-            this.tabPage1.Controls.Add(this.txtNombre);
-            this.tabPage1.Controls.Add(this.txtApellido);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -227,14 +206,6 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 369);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 81);
-            this.panel2.TabIndex = 1;
-            // 
             // tabPage3
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
@@ -244,35 +215,74 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "tabPage3";
             this.tabPage3.UseVisualStyleBackColor = true;
-
-            // button1
             // 
-            this.button1.Location = new System.Drawing.Point(243, 47);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Cancelar";
-            this.button1.UseVisualStyleBackColor = true;
+            // panel2
             // 
-            // button2
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.btnSalir);
+            this.panel2.Controls.Add(this.btnAceptar);
+            this.panel2.Controls.Add(this.btnCancelar);
+            this.panel2.Location = new System.Drawing.Point(0, 369);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 81);
+            this.panel2.TabIndex = 1;
             // 
-            this.button2.Location = new System.Drawing.Point(344, 47);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Aceptar";
-            this.button2.UseVisualStyleBackColor = true;
+            // btnSalir
             // 
-            // button3
+            this.btnSalir.Location = new System.Drawing.Point(704, 32);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
             // 
-            this.button3.Location = new System.Drawing.Point(447, 47);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Salir";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(522, 32);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 1;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(612, 32);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 0;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Controls.Add(this.Modificar);
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(108, 331);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.lblDatosPersonales);
+            this.panel3.Controls.Add(this.txtApellido);
+            this.panel3.Controls.Add(this.lblTelefono);
+            this.panel3.Controls.Add(this.txtNombre);
+            this.panel3.Controls.Add(this.lblDNI);
+            this.panel3.Controls.Add(this.txtTelefono);
+            this.panel3.Controls.Add(this.lblApellido);
+            this.panel3.Controls.Add(this.lblNombre);
+            this.panel3.Controls.Add(this.txtDni);
+            this.panel3.Location = new System.Drawing.Point(611, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(182, 344);
+            this.panel3.TabIndex = 11;
             // 
             // Form1
             // 
@@ -285,15 +295,13 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-
-
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-
             ((System.ComponentModel.ISupportInitialize)(this.dataGridPacientes)).EndInit();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -319,10 +327,11 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TabPage tabPage3;
 
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
